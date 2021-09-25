6f05c1fc9a5c5cf3940bd11413a2da7a8f79465f
Manuel <tofuah@gmail.com>
2021-09-25 20:48:11 +0100

    inserted fucntion along with home.html codes

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
 import logo from './logo.svg';
 import './App.css';
-import Header from './Header';
+import Home from './Home'
 
 function App() {
   return (
     <div>
-      <Header/>
+      <Home />
     </div>
   );
 }
---
+++ src/Home.js
@@ -0,0 +1,102 @@
+function Home() {
+  return (
+    <div>
+      <div className="back-image">
+        <div className="row logo">
+          <div className="col-md-2 moon">
+            <img className="icon" src="images/23.png" alt="logo" />
+            <div className="col-md-1 see ">
+              <h1 className="kawo">KAWO</h1>
+              <h1 className="legal">LEGAL</h1>
+            </div>
+          </div>
+          <div className="col-md-4"></div>
+          <div className="col-md-5 ">
+            <ul>
+              <div className="house">
+                <li>
+                  <a
+                    href="Home.html"
+                    className="btn -primary btn-lg active tab"
+                    role="button"
+                    aria-pressed="true"
+                  >
+                    Home
+                  </a>
+                </li>
+                <li>
+                  <a
+                    href="Startup.html"
+                    className="btn -primary btn-lg active tab"
+                    role="button"
+                    aria-pressed="true"
+                  >
+                    Startup
+                  </a>
+                </li>
+                <li>
+                  <a
+                    href="Register.html"
+                    className="btn -primary btn-lg active tab"
+                    role="button"
+                    aria-pressed="true"
+                  >
+                    Register
+                  </a>
+                </li>
+                <li>
+                  <a
+                    href="Login.html"
+                    className="btn -primary btn-lg active tab"
+                    role="button"
+                    aria-pressed="true"
+                  >
+                    Login
+                  </a>
+                </li>
+              </div>
+            </ul>
+          </div>
+        </div>
+
+        <br />
+        <br />
+        <br />
+        <br />
+
+        <div className="container">
+          <div className="row">
+            <div className="col-md-3"></div>
+            <div className="col-md-6 content">
+              <h2>KAWOLEGAL</h2>
+              <h3>
+                A collaborative ecosystem for problem solvers and support for
+                Startups.
+              </h3>
+              <br />
+              <button type="button" className="btn btn-primary">
+                Register Now!
+              </button>
+            </div>
+            <div className="col-md-3"></div>
+          </div>
+        </div>
+      </div>
+      <div className="row footer">
+        <div className="col-md-6 detail">
+          &copy; 2017 KawoLegal All Rights Reserved!
+          <br />
+        </div>
+        <div className="col-md-3"></div>
+        <div className="col-md-3 symbol">
+          <a href="google.com" className="fa fa-facebook social"></a>
+          <a href="google.com" className="fa fa-twitter social"></a>
+          <a href="google.com" className="fa fa-linkedin social"></a>
+          <a href="google.com" className="fa fa-instagram social"></a>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Home;
