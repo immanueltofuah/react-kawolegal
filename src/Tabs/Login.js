499af79156269b1a9f9a4b26e0eb39be618f9bf3
Manuel <tofuah@gmail.com>
2021-09-27 13:08:31 +0100

    copied login codes into login.js

---
+++ src/Tabs/Login.js
@@ -0,0 +1,80 @@
+function Login () {
+    return (
+    <div>
+         <div class="row ice">
+        <div class="col-md-2 tea">
+         <img class="sign" src="images/23.png" alt="logo"/>  
+         <div class="col-md-1 see">
+             <h1>KAWO</h1>
+         <h1>LEGAL</h1>
+         </div>
+         
+        </div>
+        <div class="col-md-4"></div>
+        <div class="col-md-5 ">
+            <ul>
+                <li><a href="Home.html" class="btn -primary btn-lg active cool" role="button" aria-pressed="true">Home</a></li>
+                 <li><a href="Startup.html" class="btn -primary btn-lg active cool" role="button" aria-pressed="true">Startup</a></li>
+                 <li><a href="Register.html" class="btn -primary btn-lg active cool" role="button" aria-pressed="true">Register</a></li>
+                 <li><a href="Login.html" class="btn -primary btn-lg active cool" role="button" aria-pressed="true">Login</a></li>
+
+             </ul>
+        </div>
+
+    </div>
+    
+    
+    <div class="house">
+        <br/>
+        <div class="row">
+            <div class="col-md-2"></div>
+            <div class="col-md-8">
+                <h4>Already a member? Login to add your Startup.
+                </h4>
+                <br/>
+                <br/>
+                <h6><Strong>Email Address</Strong></h6>
+                <input class="form-control form-control-lg finder" type="text" placeholder="Email" aria-label=".form-control-lg example"/>
+                <br/>
+                <h6><Strong>Password</Strong></h6>
+                <input class="form-control form-control-lg finder" type="text" placeholder="Password" aria-label=".form-control-lg example"/>
+                <br/>
+                <button type="button" class="btn btn-primary btn-lg">Sign in</button>
+                <br/>
+                <br/>
+                <br/>
+                <br/>
+                <br/>
+                <br/>
+                          
+              </div>
+
+            <div class="col-md-2"></div>
+        </div>
+        <br/>
+    </div>
+    
+    <div class="row footer">
+        <div class="col-md-6 detail"  >
+              &copy; 2017 KawoLegal All Rights Reserved!
+        <br/>
+        </div>
+        <div class="col-md-3"></div>
+        <div class="col-md-3 symbol">
+           
+
+
+
+
+              <a href="google.com"class="fa fa-facebook social"></a>
+              <a href="google.com"class="fa fa-twitter social"></a>
+              <a href="google.com"class="fa fa-linkedin social"></a>
+              <a href="google.com"class="fa fa-instagram social"></a>
+
+       </div>
+    </div>
+    </div>
+    )
+}
+
+export default Login;
