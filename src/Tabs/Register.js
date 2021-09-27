f733ee551e4cae4b816a912a281a45bff709c155
Manuel <tofuah@gmail.com>
2021-09-27 12:42:11 +0100

    placed a closing tag in img input

--- src/Tabs/Register.js
+++ src/Tabs/Register.js
@@ -3,7 +3,7 @@ function Register () {
         <div>
         <div class="row ice">
         <div class="col-md-2 tea">
-         <img class="sign" src="images/23.png" alt="logo">  
+         <img class="sign" src="images/23.png" alt="logo"/>  
          <div class="col-md-1 see">
              <h1>KAWO</h1>
          <h1>LEGAL</h1>
@@ -27,37 +27,37 @@ function Register () {
   
     
     <div class="house">
-        <br>
+        <br/>
         <div class="row">
             <div class="col-md-2"></div>
             <div class="col-md-8">
                 <h4>Join KawoLegal. Sign up to get your Startup listed now!
                 </h4>
-                <br>
-                <br>
+                <br/>
+                <br/>
                 <h6><Strong>Full Name</Strong></h6>
-                <input class="form-control form-control-lg finder" type="text" placeholder="Full Name" aria-label=".form-control-lg example">
-                <br>
+                <input class="form-control form-control-lg finder" type="text" placeholder="Full Name" aria-label=".form-control-lg example"/>
+                <br/>
                 <h6><Strong>Email Address</Strong></h6>
-                <input class="form-control form-control-lg finder" type="text" placeholder="Email" aria-label=".form-control-lg example">
-                <br>
+                <input class="form-control form-control-lg finder" type="text" placeholder="Email" aria-label=".form-control-lg example"/>
+                <br/>
                 <h6><Strong>Password</Strong></h6>
-                <input class="form-control form-control-lg finder" type="text" placeholder="Password" aria-label=".form-control-lg example">
-                <br>
+                <input class="form-control form-control-lg finder" type="text" placeholder="Password" aria-label=".form-control-lg example"/>
+                <br/>
                 <h6><Strong>Confirm Password</Strong></h6>
-                <input class="form-control form-control-lg finder" type="text" placeholder="Confirm Password" aria-label=".form-control-lg example">
-                <br>
+                <input class="form-control form-control-lg finder" type="text" placeholder="Confirm Password" aria-label=".form-control-lg example"/>
+                <br/>
                 <button type="button" class="btn btn-primary btn-lg">Sign in</button>            </div>
 
             <div class="col-md-2"></div>
         </div>
-        <br>
+        <br/>
     </div>
     
     <div class="row footer">
         <div class="col-md-6 detail"  >
               &copy; 2017 KawoLegal All Rights Reserved!
-        <br>
+        <br/>
         </div>
         <div class="col-md-3"></div>
         <div class="col-md-3 symbol">
