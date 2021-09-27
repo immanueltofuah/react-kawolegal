24835157f6822ca5e5b56b8423893c4991a9e60b
Manuel <tofuah@gmail.com>
2021-09-27 12:32:32 +0100

    imported Startup in app.js

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import logo from './logo.svg';
 import './App.css';
 import Home from './Tabs/Home';
+import Startup from './Tabs/Startup';
 
 function App() {
   return (
     <div>
       <Home />
+      <Startup />
     </div>
   );
 }
