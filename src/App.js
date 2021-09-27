f5e28fb7855d07cc47cdf7b320c66dcc8b48bbca
Manuel <tofuah@gmail.com>
2021-09-27 13:43:42 +0100

    changed titile to Kawo Legal

--- src/App.js
+++ src/App.js
@@ -1,17 +1,6 @@
 import logo from './logo.svg';
-import './App.css';
-import Home from './Tabs/Home';
-import Startup from './Tabs/Startup';
-import Register from './Tabs/Register';
+
 
 function App() {
-  return (
-    <div>
-      <Home />
-      <Startup />
-      <Register />
-    </div>
-  );
+  
 }
-
-export default App;
---
+++ src/Router.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import { BrowserRouter, Route } from "react-router-dom";
+import Home from './Tabs/Home';
+import Startup from './Tabs/Startup';
+import Register from './Tabs/Register';
+import Login from './Tabs/Login';
+
+function Router(Props) {
+    <BrowserRouter>
+    <Route exact path="/" component={Home} />
+    <Route exact path="/startups" component={Startup} />
+    <Route exact path="/register" component={Register} />
+    <Route exact path="/login" component={Login} />
+    
+    
+    </BrowserRouter>
+
+}
